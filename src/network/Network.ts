7ddb04f1735a5373272ae6cd60f0b14beab0cb16
pocholo818 <89579920+pocholo818@users.noreply.github.com>
2023-02-27 23:51:42 +0800

    Update Network.ts

--- src/network/Network.ts
+++ src/network/Network.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://bocfp-api.vercel.app",
+  baseURL: "https://bocfp-api.vercel.app",
   headers: {
     "Content-Type": "application/json"
   },
